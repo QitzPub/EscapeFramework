85732a3d4e5f869ebf5f4fa7dd9b2c96871284f5
hiraken123 <ikauka2@gmail.com>
2019-07-25 20:14:51 +0900

    最初

---
+++ EscapeFramework/Assets/Scripts/NextSceneButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof (Button))]
+public class NextSceneButton : MonoBehaviour
+{
+
+    [SerializeField]
+    string SceneName;
+    public void GotoScene()
+    {
+        SceneManager.LoadScene(SceneName);
+        
+    }
+}
