8038be912b2c72974437f016a73831f02433433b
USER <user@USERnoMacBook-Pro.local>
2019-09-06 17:17:32 +0900

    ADVのシステム初期化作成

---
+++ EscapeFramework/Assets/EscapeGameFrameWork/ADVEngine/Scripts/GameSystemInitializer/ADVSystemInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Qitz.ArchitectureCore.ADVGame;
+using UnityEngine;
+namespace Qitz.ADVGame
+{
+    public class ADVSystemInitializer
+    {
+        [RuntimeInitializeOnLoadMethod]
+        public static void ADVSystemInitialize()
+        {
+
+            var ga = new GameObject();
+            var controller = PrefabFolder.ResourcesLoadInstantiateTo("ADVSystem", ga.transform.parent);
+            Object.Destroy(ga);
+            Object.DontDestroyOnLoad(controller);
+        }
+    }
+}
