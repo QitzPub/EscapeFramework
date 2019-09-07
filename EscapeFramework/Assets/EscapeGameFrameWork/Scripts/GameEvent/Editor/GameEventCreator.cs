bd2db91a5c01faed134d47cd2aa85df5a52455e4
USER <user@USERnoMacBook-Pro.local>
2019-09-07 11:45:02 +0900

    update

---
+++ EscapeFramework/Assets/EscapeGameFrameWork/Scripts/GameEvent/Editor/GameEventCreator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Qitz.EscapeFramework
+{
+    public class GameEventCreator
+    {
+
+        [MenuItem("GameObject/QitzEscapeGameEvent", priority = 21)]
+        public static void GameObjectMenuItem()
+        {
+            Debug.Log("GameObjectMenuItem");
+            PrefabFolder.ResourcesLoadInstantiateTo("EscapeGameEvent", Selection.activeGameObject.transform);
+
+        }
+
+    }
+}
